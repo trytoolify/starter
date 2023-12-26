c0f8e6637aab962e0d8aa21b547feefa51cf27cd
Rohan Mayya <rohan.mayya@gmail.com>
2023-12-26 16:01:27 +0530

    bump

--- tools/list.tsx
+++ tools/list.tsx
@@ -5,7 +5,6 @@ async function List() {
   const testFetch = await fetch(
     "https://jsonplaceholder.typicode.com/todos/1"
   ).then((res) => res.json());
-  console.log("testFetch", testFetch);
 
   const groceryList = [
     "Eggs",
@@ -22,15 +21,15 @@ async function List() {
   ];
 
   return (
-    <Block id='list' w={1} h={3}>
-      <div className='flex flex-col p-4 px-2 relative'>
-        <div className='px-2 mb-2 text-neutral-400 text-xs'> Grocery List </div>
+    <Block id="list" w={1} h={3}>
+      <div className="flex flex-col p-4 px-2 relative">
+        <div className="px-2 mb-2 text-neutral-400 text-xs"> Grocery List </div>
 
         {groceryList.map((item, index) => {
           return (
             <Button
               key={index}
-              className='justify-start w-full'
+              className="justify-start w-full"
               variant={"ghost"}
             >
               {item}

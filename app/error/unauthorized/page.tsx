c883b833ba2f07adf3415b8252fed26849509f94
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2024-03-02 15:30:27 -0800

    jwt verification

---
+++ app/error/unauthorized/page.tsx
@@ -0,0 +1,7 @@
+export default function AccessDenied() {
+    return (
+        <div className="flex items-center justify-center h-[calc(100vh-108px)]">
+            <p>Access Denied.</p>
+        </div>
+    );
+}

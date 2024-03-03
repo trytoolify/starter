f6527233a87bd2dcb92c8e29d4b01ea90ff1d360
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2024-03-03 14:46:23 -0800

    apos

--- app/error/unauthorized/page.tsx
+++ app/error/unauthorized/page.tsx
@@ -1,7 +1,7 @@
 export default function AccessDenied() {
     return (
         <div className="flex items-center justify-center h-[calc(100vh-108px)]">
-            <p>Sorry, your access token has expired or you don't have the necessary permissions to access this
+            <p>Sorry, your access token has expired or you don&apos;t have the necessary permissions to access this
                 content.</p>
         </div>
     );

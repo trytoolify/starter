34c41d28530e74aca07173bd58783560373f8ae7
Rohan Mayya <rohan.mayya@gmail.com>
2024-03-02 16:25:47 -0800

    Revert "jwt verification"
    This reverts commit c883b833ba2f07adf3415b8252fed26849509f94.

--- app/error/unauthorized/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AccessDenied() {
-    return (
-        <div className="flex items-center justify-center h-[calc(100vh-108px)]">
-            <p>Access Denied.</p>
-        </div>
-    );
-}

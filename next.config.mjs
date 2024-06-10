5afc453bb2f2edafcbeb7d0d6360a37529caa906
saif <saif@trycreo.com>
2024-06-10 17:30:21 +0530

    base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["@trycreo/ui"],
     ...(process.env.CREO_ENV === "editor" && {
         basePath: `/${process.env.SESSION_BACKEND_STATIC_TOKEN}`,
     }),
+    env: {
+        BASE_PATH:
+            process.env.CREO_ENV === "editor"
+                ? `/${process.env.SESSION_BACKEND_STATIC_TOKEN}`
+                : "",
+    },
 };
 
 export default nextConfig;

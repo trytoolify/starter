edfbcd13a8cb66ef6616930fc9dbb221dcc89536
Saif Ur Rahman <saifsmailbox98@users.noreply.github.com>
2024-06-29 09:19:15 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 const nextConfig = {
-    transpilePackages: ["@trycreo/ui"],
-    ...(process.env.CREO_ENV === "editor" && {
-        basePath: `/${process.env.SESSION_BACKEND_STATIC_TOKEN}`,
-    }),
-    env: {
-        BASE_PATH:
-            process.env.CREO_ENV === "editor"
-                ? `/${process.env.SESSION_BACKEND_STATIC_TOKEN}`
-                : "",
-    },
+    transpilePackages: ["@trycreo/ui"]
 };
 
 export default nextConfig;
